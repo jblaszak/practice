eac92debe329c85080a355516865e1332e454eae
Jo Blaszak <jblaszak@gmail.com>
2022-08-30 14:29:10 -0700

    Added empty submit handling for interactive-rating-component challenge.

--- frontendmentor/level-one/interactive-rating-component-main/index.js
+++ frontendmentor/level-one/interactive-rating-component-main/index.js
@@ -1,34 +1,48 @@
 const submitHandler = (event) => {
   event.preventDefault();
 
-  const card = document.getElementsByClassName("rating-card")[0];
-  card.classList.add("thanks-card");
-
-  const thanksIllustration = document.createElement("img");
-  thanksIllustration.src = "./images/illustration-thank-you.svg";
-  thanksIllustration.alt = "";
-  thanksIllustration.ariaHidden = true;
-  card.prepend(thanksIllustration);
-
   const formData = new FormData(event.target);
   const formProps = Object.fromEntries(formData);
-  const ratingValue = formProps.rating;
+  const ratingValue = formProps?.rating;
 
-  const rating = document.getElementsByClassName("star")[0];
-  rating.classList.remove("star");
-  rating.classList.add("final-rating");
-  rating.textContent = `You selected ${ratingValue} out of 5`;
+  if (ratingValue != null) {
+    const card = document.getElementsByClassName("rating-card")[0];
+    card.classList.add("thanks-card");
 
-  const heading = document.getElementById("heading");
-  heading.textContent = `Thank you!`;
-  heading.classList.add("final-heading");
+    const thanksIllustration = document.createElement("img");
+    thanksIllustration.src = "./images/illustration-thank-you.svg";
+    thanksIllustration.alt = "";
+    thanksIllustration.ariaHidden = true;
+    card.prepend(thanksIllustration);
 
-  const text = document.getElementById("text");
-  text.textContent = `We appreciate you taking the time to give a rating. If you ever need more support, 
-  don’t hesitate to get in touch!`;
+    const rating = document.getElementsByClassName("star")[0];
+    rating.classList.remove("star");
+    rating.classList.add("final-rating");
+    rating.textContent = `You selected ${ratingValue} out of 5`;
 
-  form.remove();
+    const heading = document.getElementById("heading");
+    heading.textContent = `Thank you!`;
+    heading.classList.add("final-heading");
+
+    const text = document.getElementById("text");
+    text.textContent = `We appreciate you taking the time to give a rating. If you ever need more support, 
+    don’t hesitate to get in touch!`;
+
+    form.remove();
+  }
 };
 
 const form = document.getElementById("form");
-form.addEventListener("submit", submitHandler);
+form.addEventListener("submit", submitHandler, { once: true });
+
+const fieldset = document.getElementsByClassName("ratings")[0];
+fieldset.addEventListener(
+  "click",
+  function (event) {
+    if (event.target !== this) {
+      const button = document.getElementById("submit");
+      button.removeAttribute("disabled");
+    }
+  },
+  { once: true }
+);
