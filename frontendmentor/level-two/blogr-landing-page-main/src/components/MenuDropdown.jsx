b9422f10c23ba35b8b0495b9415306a91fee6c04
Jo Blaszak <jblaszak@gmail.com>
2022-09-09 22:56:30 -0700

    Added functioning dropdowns to mobile menu.

--- frontendmentor/level-two/blogr-landing-page-main/src/components/MenuDropdown.jsx
+++ frontendmentor/level-two/blogr-landing-page-main/src/components/MenuDropdown.jsx
@@ -1,9 +1,12 @@
 import "./MenuDropdown.css";
+import { useState } from "react";
 
 export default function MenuDropdown({ category, items }) {
+  const [open, setOpen] = useState(false);
+
   return (
-    <li className="dropdown">
-      <span>{category}</span>
+    <li className={`dropdown${open ? " open" : ""}`}>
+      <span onClick={() => setOpen((prev) => !prev)}>{category}</span>
       <ul>
         {items.map((item) => {
           return (
