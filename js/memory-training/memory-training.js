a6986db4b7c1fc2ad63ab8c42d14dd65d665c559
Jo Blaszak <jblaszak@gmail.com>
2022-09-29 11:04:04 -0700

    Finished memory training app

--- js/memory-training/memory-training.js
+++ js/memory-training/memory-training.js
@@ -1,33 +1,75 @@
-let sequences = [];
-const usedSequences = [];
-const guesses = [];
+let sequences;
+let usedSequences;
+let guesses;
 const entry = document.getElementById("entry");
+const form = document.getElementById("answer-form");
+const result = document.getElementById("result");
+const startButton = document.getElementById("start");
 
 const start = () => {
+  sequences = [];
+  usedSequences = [];
+  guesses = [];
+  result.textContent = "";
+
   while (sequences.length < 20) {
     // Generate new 3 digit hexadecimal number
-    const newSequence = Math.floor(Math.random() * 4096).toString(16);
+    const newSequence = Math.floor(Math.random() * 4096)
+      .toString(16)
+      .padStart(3, "0");
     if (!sequences.includes(newSequence)) {
       sequences.push(newSequence);
     }
   }
 
-  let intervalCount = 0;
+  let newSequence = getSequence();
+  usedSequences.push(newSequence);
+  entry.textContent = newSequence;
+
+  let intervalCount = 1;
 
   const interval = setInterval(() => {
     intervalCount++;
-    if (intervalCount === 6) {
-      entry.textContent = "";
-      clearInterval(interval);
-    } else {
+
+    const getSequence = () => {
       const entryIndex = Math.floor(Math.random() * sequences.length);
-      entry.textContent = sequences[entryIndex];
-      usedSequences.push(sequences[entryIndex]);
-      sequences = sequences.filter((val, index) => index !== entryIndex);
+      if (usedSequences.includes(sequences[entryIndex])) {
+        return getSequence();
+      }
+      return sequences[entryIndex];
+    };
+
+    if (intervalCount < 6) {
+      newSequence = getSequence();
+      usedSequences.push(newSequence);
+      entry.textContent = newSequence;
+    } else {
+      entry.textContent = "\xa0";
+      clearInterval(interval);
+      fillAnswers();
     }
-  }, 1000);
+  }, 4000);
 };
 
-start();
+const fillAnswers = () => {
+  const answerList = document.getElementById("answer-list");
+  for (let i = 0; i < answerList.children.length; i++) {
+    answerList.children[i].children[0].textContent = sequences[i];
+    answerList.children[i].children[1].name = sequences[i];
+  }
+};
 
-console.log(sequences);
+const checkAnswers = (e) => {
+  e.preventDefault();
+  const formValues = [...new FormData(form).entries()];
+  const answers = formValues.map((entry) => entry[0]);
+
+  const numCorrect = answers.reduce((acc, curr) => {
+    return acc + usedSequences.includes(curr);
+  }, 0);
+
+  result.textContent = `You got ${numCorrect} / 5 correct!`;
+};
+
+form.addEventListener("submit", checkAnswers);
+startButton.addEventListener("click", start);
