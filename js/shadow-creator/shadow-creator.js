ea03009fb804045d21e92132a09bd126414c65ed
Jo Blaszak <jblaszak@gmail.com>
2022-10-12 22:25:19 -0700

    Cleaned up controls to be more responsive and positioning of joystick on shadow-creator

--- js/shadow-creator/shadow-creator.js
+++ js/shadow-creator/shadow-creator.js
@@ -25,6 +25,12 @@ const onChangeInput = () => {
   preview.style.boxShadow = shadow;
 };
 
+document.addEventListener("input", (e) => {
+  // if (e.target !== this) {
+  onChangeInput();
+  // }
+});
+
 document.addEventListener("mousemove", (e) => {
   if (e.target !== this) {
     onChangeInput();
@@ -33,8 +39,10 @@ document.addEventListener("mousemove", (e) => {
 
 const moveJoystick = (e) => {
   e.preventDefault();
-  joystickX = e.clientX <= minX ? -1 : e.clientX > maxX ? maxX - minX - 1 : e.clientX - minX;
-  joystickY = e.clientY <= minY ? -1 : e.clientY > maxY ? maxY - minY - 1 : e.clientY - minY;
+  const currX = e.clientX - joystick.clientWidth / 2;
+  const currY = e.clientY - joystick.clientWidth / 2;
+  joystickX = currX <= minX ? -1 : currX > maxX ? maxX - minX - 1 : currX - minX;
+  joystickY = currY <= minY ? -1 : currY > maxY ? maxY - minY - 1 : currY - minY;
 
   joystick.style.left = `${joystickX}px`;
   joystick.style.top = `${joystickY}px`;
@@ -42,11 +50,11 @@ const moveJoystick = (e) => {
 
 joystick.addEventListener("mousedown", (e) => {
   e.preventDefault();
-  joystick.style.cursor = "all-scroll";
+  joystick.style.cursor = "grabbing";
   document.addEventListener("mousemove", moveJoystick);
 });
 
 document.addEventListener("mouseup", (e) => {
-  joystick.style.cursor = "pointer";
+  joystick.style.cursor = "grab";
   document.removeEventListener("mousemove", moveJoystick);
 });
